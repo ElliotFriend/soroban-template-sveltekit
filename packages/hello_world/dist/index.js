9574c5972371ca300295375c42c2bd4d7651b672
Elliot Voris <elliot@stellar.org>
2025-09-04 10:37:22 -0500

    re-deploy and re-bind hello world contract

--- packages/hello_world/dist/index.js
+++ packages/hello_world/dist/index.js
@@ -1,8 +1,5 @@
 import { Buffer } from 'buffer';
 import { Client as ContractClient, Spec as ContractSpec } from '@stellar/stellar-sdk/contract';
-export * from '@stellar/stellar-sdk';
-export * as contract from '@stellar/stellar-sdk/contract';
-export * as rpc from '@stellar/stellar-sdk/rpc';
 if (typeof window !== 'undefined') {
     //@ts-ignore Buffer exists
     window.Buffer = window.Buffer || Buffer;
@@ -10,12 +7,17 @@ if (typeof window !== 'undefined') {
 export const networks = {
     testnet: {
         networkPassphrase: 'Test SDF Network ; September 2015',
-        contractId: 'CAYI2VDGAAK6LOPPNQEVO2CSX5IPKSOGGQDTSGHXFKL5DZKPEAHYSAO4',
+        contractId: 'CDB4B5COHVSC4ELUWHELFUF7SPIUURXK6L6D3D3NJGHIVXQAWD73GLVY',
     },
 };
-export const Errors = {};
 export class Client extends ContractClient {
     options;
+    static async deploy(
+        /** Options for initializing a Client as well as for calling a method, with extras specific to deploying. */
+        options,
+    ) {
+        return ContractClient.deploy(null, options);
+    }
     constructor(options) {
         super(
             new ContractSpec([
--- packages/hello_world/src/index.ts
+++ packages/hello_world/src/index.ts
@@ -1,28 +1,11 @@
 import { Buffer } from 'buffer';
-import { Address } from '@stellar/stellar-sdk';
 import {
     AssembledTransaction,
     Client as ContractClient,
     ClientOptions as ContractClientOptions,
-    Result,
+    MethodOptions,
     Spec as ContractSpec,
 } from '@stellar/stellar-sdk/contract';
-import type {
-    u32,
-    i32,
-    u64,
-    i64,
-    u128,
-    i128,
-    u256,
-    i256,
-    Option,
-    Typepoint,
-    Duration,
-} from '@stellar/stellar-sdk/contract';
-export * from '@stellar/stellar-sdk';
-export * as contract from '@stellar/stellar-sdk/contract';
-export * as rpc from '@stellar/stellar-sdk/rpc';
 
 if (typeof window !== 'undefined') {
     //@ts-ignore Buffer exists
@@ -32,11 +15,9 @@ if (typeof window !== 'undefined') {
 export const networks = {
     testnet: {
         networkPassphrase: 'Test SDF Network ; September 2015',
-        contractId: 'CAYI2VDGAAK6LOPPNQEVO2CSX5IPKSOGGQDTSGHXFKL5DZKPEAHYSAO4',
+        contractId: 'CDB4B5COHVSC4ELUWHELFUF7SPIUURXK6L6D3D3NJGHIVXQAWD73GLVY',
     },
 } as const;
-
-export const Errors = {};
 
 export interface Client {
     /**
@@ -63,6 +44,20 @@ export interface Client {
     ) => Promise<AssembledTransaction<Array<string>>>;
 }
 export class Client extends ContractClient {
+    static async deploy<T = Client>(
+        /** Options for initializing a Client as well as for calling a method, with extras specific to deploying. */
+        options: MethodOptions &
+            Omit<ContractClientOptions, 'contractId'> & {
+                /** The hash of the Wasm blob, which must already be installed on-chain. */
+                wasmHash: Buffer | string;
+                /** Salt used to generate the contract's ID. Passed through to {@link Operation.createCustomContract}. Default: random. */
+                salt?: Buffer | Uint8Array;
+                /** The format used to decode `wasmHash`, if it's provided as a string. */
+                format?: 'hex' | 'base64';
+            },
+    ): Promise<AssembledTransaction<T>> {
+        return ContractClient.deploy(null, options);
+    }
     constructor(public readonly options: ContractClientOptions) {
         super(
             new ContractSpec([
