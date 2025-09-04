4aa689f560e3b0cfb8ac41302e35965e4887d142
Elliot Voris <elliot@stellar.org>
2025-09-04 10:53:37 -0500

    add a simple test for connect buttons

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,10 @@ test('index page has expected h1', async ({ page }) => {
     await page.goto('/');
     await expect(page.getByRole('heading', { name: 'Hello, Stellar SvelteKit!' })).toBeVisible();
 });
+
+test('index page has expected connect buttons', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByRole('button', { name: 'Signup' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible();
+})
